158e649e169eeffc0228a42791a56056ac576ede
júniorflima <junior_618@hotmail.com>
2024-03-31 10:11:51 -0300

    Adicionar arquivos de projeto.

---
+++ Desenvolvimento/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Desenvolvimento.Models;
+
+namespace Desenvolvimento.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Desenvolvimento.Models.Alunos> Alunos { get; set; } = default!;
+    }
+}
---
+++ Desenvolvimento/Models/Alunos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+namespace Desenvolvimento.Models
+{
+    public class Alunos
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
